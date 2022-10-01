86683ce38ad9c52875f35da0738e3df23d485694
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2022-10-01 16:32:43 +0200

    challenge 1

--- app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.androidconcurrency2020
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidconcurrency2020.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var imageViews: Array<ImageView>
+    private lateinit var drawables: Array<Int>
+    val handler = object : Handler(){
+        override fun handleMessage(msg: Message) {
+            val data = msg.data
 
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,9 +35,8 @@ class MainActivity : AppCompatActivity() {
      * Run some code
      */
     private fun rollTheDice() {
-
-        val imageViews = arrayOf(binding.die1, binding.die2, binding.die3, binding.die4, binding.die5)
-        val drawables = arrayOf(
+        imageViews = arrayOf(binding.die1, binding.die2, binding.die3, binding.die4, binding.die5)
+        drawables = arrayOf(
             R.drawable.die_1,
             R.drawable.die_2,
             R.drawable.die_3,
@@ -34,10 +44,15 @@ class MainActivity : AppCompatActivity() {
             R.drawable.die_5,
             R.drawable.die_6
         )
-
+        val bundle = Bundle()
         for (dieIndex in imageViews.indices) {
             val dieNumber = getDieValue()
-            imageViews[dieIndex].setImageResource(drawables[dieNumber - 1])
+            /*imageViews[dieIndex].setImageResource(drawables[dieNumber - 1])*/
+            bundle.putInt("", dieNumber)
+            Message().also {
+                it.data = bundle
+                handler.sendMessage(it)
+            }
         }
 
     }
